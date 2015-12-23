581c6ce8aec0f61dfc173197202c3c32e1d2ac06
Wouter van Harten <woutervh@live.nl>
2015-12-24 00:29:43 +0100

    Made basic session login for the map and groups

--- sendMessage.php
+++ sendMessage.php
@@ -1,4 +1,4 @@
 <?php
 $message = $_POST["group"]." ".$_POST["message"];
-file_put_contents(getcwd()."/message.txt", $message, FILE_APPEND);
-include "index.php";
+file_put_contents(getcwd()."/message.txt", $message."\n", FILE_APPEND);
+header('Location: index.php');
